31c21d2b7d3b8c5671164c5c862eb956d58ced41
Roberto Castro <30873841+robertocastro7@users.noreply.github.com>
2020-03-24 20:45:18 -0600

    Create testexec.c

---
+++ testexec.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(){
+    int pid;
+    pid = fork();
+    if(pid == 0){
+        execlp("ls", "ls","-a","/", (char *)NULL);
+    }
+    else{
+        printf("Soy el padre.\n");
+        
+    }
+}
