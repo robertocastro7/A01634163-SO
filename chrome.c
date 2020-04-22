c34b04c879919551741a772db3fa3a3aba1bfb05
Roberto Castro <30873841+robertocastro7@users.noreply.github.com>
2020-04-21 18:14:48 -0600

    Update chrome.c

--- chrome.c
+++ chrome.c
@@ -13,7 +13,7 @@ int main(){
     hijos[i] = pid;
     if(pid != 0){
       shmId = smhget(200, 400, 0644);
-      char * var = (int *)shmat(smhId, NULL, 0);
+      int * var = (int *)shmat(smhId, NULL, 0);
       var = &var[i*10];
       for(int j = 0; j < 10; j++){
         var[j] = i + '0';
@@ -24,8 +24,8 @@ int main(){
   for(int i = 0; i < 5; i++){
     waitpid(hijos[i], 0, 0);
   }
-  char * var = (int *)shmat(smhId, NULL, 0);
-  for(int i = 0; i < 400; i++){
+  int * var = (int *)shmat(smhId, NULL, 0);
+  for(int i = 0; i < 100; i++){
     printf("El contenido es %d\n", var[i]);
   }
   return 0;
