5b7194ee1cca6ad446b1836654f11f726e4415fa
Roberto Castro <30873841+robertocastro7@users.noreply.github.com>
2020-04-21 19:47:18 -0600

    Create thread.c

---
+++ thread.c
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#define NUMTHREADS 5
+
+
+int val = 5
+pthread_mutex_t mutex;
+
+void * hello(void *id){
+  pthread_mutex_lock(&mutex);
+  printf("Hello world %ld\n", (long)id, val);
+  val = val+;
+  pthread_mutex_unlock(&mutex);
+};
+
+
+int main(){
+  pthread_t hilos[NUMTHREADS];
+  for(long i = 0; i < NUMTHREADS; i++){
+    pthread_create(&hilos[i], NULL, hello, (void *)i); 
+  }
+  pthread_exit(NULL);
+}
